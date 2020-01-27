d52f4bf923bc4ee9e05ddc386fc76c136123823c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-01-27 07:57:58 -0600

    Update transition.py

--- transition.py
+++ transition.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
-use pyspark;
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import plot_roc_curve
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+
+X, y = load_wine(return_X_y=True)
+y = y == 2
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+svc = SVC(random_state=42)
+svc.fit(X_train, y_train)
+
+
 
 
