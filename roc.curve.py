8f4c520b711526dd33f436c142b7f6f217a027c8
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-01-27 08:16:51 -0600

    Create roc.curve.py

---
+++ roc.curve.py
@@ -0,0 +1,8 @@
+python3
+
+rfc = RandomForestClassifier(n_estimators=10, random_state=42)
+rfc.fit(X_train, y_train)
+ax = plt.gca()
+rfc_disp = plot_roc_curve(rfc, X_test, y_test, ax=ax, alpha=0.8)
+svc_disp.plot(ax=ax, alpha=0.8)
+plt.show()
