bd409efc3f77264a54509a91f4fbd328920a341e
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-09-01 16:45:00 -0500

    Update transition.py

--- transition.py
+++ transition.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 
 X, y = load_wine(return_X_y=True)
 y = y == 2
-
+print 1,2,3,4,5,6,7,8,9,10
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 svc = SVC(random_state=42)
 svc.fit(X_train, y_train)
