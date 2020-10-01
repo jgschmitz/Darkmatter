d65efe1914bd2f0a375784cf0d8b58993ea146b0
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-10-01 08:11:41 -0500

    Update transition.py

--- transition.py
+++ transition.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import plot_roc_curve
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
-X, y = load_
+X, y = lo
 X, y = load_wine(return_X_y=True)
 y = y == 2
 
