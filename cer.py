2b78acc7ed4124bc8b8dd2a7c9d5abfc0d0931e4
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-01-31 07:31:49 -0600

    Update cer.py

--- cer.py
+++ cer.py
@@ -1,7 +1,7 @@
 print(__doc__)
 from sklearn.linear_model import LogisticRegression
 from sklearn import set_config
-p
+
 lr = LogisticRegression(penalty='l1')
 print('Default representation:')
 print(lr)
