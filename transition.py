e3e5449410eeecc8eb9cc98d419100580345f9aa
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-05-28 10:23:27 -0500

    Update transition.py

--- transition.py
+++ transition.py
@@ -1,7 +1,7 @@
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import plot_roc_curve
-PRINT 1,2,3,4
+PRINT 1,2,3
 X, y = load_wine(return_X_y=True)
 y = y == 2
 
