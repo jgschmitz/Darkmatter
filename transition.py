8a2913cd9bd1019a5ca318776a65fcc780140ce1
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-06-01 16:27:07 -0500

    Update transition.py

--- transition.py
+++ transition.py
@@ -4,7 +4,7 @@ from sklearn.metrics import plot_roc_curve
 
 X, y = load_wine(return_X_y=True)
 y = y == 2
-pr
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 svc = SVC(random_state=42)
 svc.fit(X_train, y_train)
