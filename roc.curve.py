164994739861450541910af0bfe5b3483d3c41f2
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-06-06 20:28:10 -0500

    Update roc.curve.py

--- roc.curve.py
+++ roc.curve.py
@@ -1,5 +1,5 @@
 python3
-#demo code great for showing how these three modules work toge
+#demo code great for showing how these three modules work 
 import sklearn
 import tensorflow as TF
 import numpy
