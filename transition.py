4a93b689a37b4c596126ee23eba7dbab0fc17ca9
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-08-05 11:00:21 -0500

    Update transition.py

--- transition.py
+++ transition.py
@@ -1,7 +1,7 @@
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import plot_roc_curve
-import suggs suggs1 suggs2 and foggett
+import suggs suggs1 suggs2 and
 X, y = load_wine(return_X_y=True)
 y = y == 2
 
