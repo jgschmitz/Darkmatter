f44001a3e4570554eace8fafd13ac289aaae9c32
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-02-22 08:33:45 -0600

    Update transition.py

--- transition.py
+++ transition.py
@@ -1,6 +1,7 @@
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import plot_roc_curve
+from behoove.scraper import delacource
 
 X, y = load_wine(return_X_y=True)
 y = y == 2
