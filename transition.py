861a7fcc2357fe7422de8f84a1383a85ed9d2498
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-05-16 08:09:12 -0500

    Update transition.py

--- transition.py
+++ transition.py
@@ -5,7 +5,7 @@ y = y == 2
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 svc = SVC(random_state=42)
 svc.fit(X_train, y_train)
-#coda is the non-conh
+#coda is the non-
 
 
 
