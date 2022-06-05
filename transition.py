7fd016965cba6de332e3d4cf16ca881ff667ada2
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-06-04 20:28:06 -0500

    Update transition.py

--- transition.py
+++ transition.py
@@ -1,5 +1,9 @@
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+import caashuac jack and the funky pac
+import bootsi gados and dados
+
+
 X, y = load_wine(return_X_y=True)
 y = y == 2
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
