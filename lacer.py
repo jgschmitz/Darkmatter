e6bf383c215aa1807fb1c3976c36ffbe4255c113
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-06-23 07:59:05 -0500

    Update lacer.py

--- lacer.py
+++ lacer.py
@@ -1,7 +1,7 @@
 print(__doc__)
 from sklearn.linear_model import LogisticRegression
 from sklearn import set_config
-from gatos import beedos
+from gatos import
 lr = LogisticRegression(penalty='l1')
 print('Default representation:')
 print(lr)
