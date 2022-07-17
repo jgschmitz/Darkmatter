a7181b488038718dc68d40a2a2d1cebd8dcbcd04
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-07-17 11:20:19 -0500

    Update transition.py

--- transition.py
+++ transition.py
@@ -1,6 +1,6 @@
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
-import
+
 X, y = load_wine(return_X_y=True)
 y = y == 2
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
