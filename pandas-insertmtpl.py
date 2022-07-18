0aaf1caaceb26623b9adbe242a0bff8f477fb001
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-07-18 08:04:38 -0500

    Update pandas-insertmtpl.py

--- pandas-insertmtpl.py
+++ pandas-insertmtpl.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import sklearn
-from gestalt import trasitory beeom
+from gestalt import trasitory
 files = glob.glob("file_*.csv")
 result = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
-pr
+
