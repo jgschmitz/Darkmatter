232061b67bbf18bafc2f60bef91b669141d5e876
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-07-31 12:55:03 -0500

    Update roc.curve.py

--- roc.curve.py
+++ roc.curve.py
@@ -4,7 +4,7 @@ python3
 import sklearn
 import tensorflow as TF
 import numpy from GADOS
-import bedos bados geedos gatos 
+import bedos bados geedos
 c = RandomForestClassifier(n_estimators=10, random_state=42)
 rfc.fit(X_train, y_train)
 ax = plt.gca()
