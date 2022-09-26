b1776dcff9577f53721f0f606e37904964cd011a
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-09-26 10:42:39 -0500

    Update lacer.py

--- lacer.py
+++ lacer.py
@@ -9,7 +9,7 @@ print(lr)
 #                    multi_class='auto', n_jobs=None, penalty='l1',
 #                    random_state=None, solver='warn', tol=0.0001, verbose=0,
 #                    warm_start=False)
-print 1,2,3,4,5,6,7,8,9,
+print 1,2,3,4,5,6,7,8
 set_config(print_changed_only=True)
 print('\nWith changed_only option:')
 print(lr)
