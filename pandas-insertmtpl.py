79b820b01d09ededb0da186e2b3f3be3131473e1
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-09-28 08:26:20 -0500

    Update pandas-insertmtpl.py

--- pandas-insertmtpl.py
+++ pandas-insertmtpl.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import sklearn
-print 1,2,3,4,5,6,7,8,9,10
+print 1,2,3,4,5,6,7,8,
 files = glob.glfile_*.csv")
 result = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
 
