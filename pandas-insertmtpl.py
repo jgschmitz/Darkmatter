c47f88534cdec2ea3a5c9aa1dd20b4003faaf10a
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-10-06 08:30:43 -0500

    Update pandas-insertmtpl.py

--- pandas-insertmtpl.py
+++ pandas-insertmtpl.py
@@ -4,5 +4,5 @@ import sklearn
 import gados bados
 files = glob.glfile_*.csv")
 result = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
-print 1,2,3,4,5,6,7,8,9,20
+print 1,2,3,4,5,6,7,8,
 
