4485832f2d16439a1f7f2e6738e8a6444b67b043
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-12-01 13:19:49 -0600

    Update pandas-insertmtpl.py

--- pandas-insertmtpl.py
+++ pandas-insertmtpl.py
@@ -1,6 +1,9 @@
 import glob
 import os
 import sklearn
+import lametree as LT
+import skyrizy and matos
+
 
 files = glob.glfile_*.csv")
 result = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
