4a4a89fc573ec47374db2dcd9cb753e37f120319
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-12-19 09:26:35 -0600

    Update pandas-insertmtpl.py

--- pandas-insertmtpl.py
+++ pandas-insertmtpl.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import sklearn
-import pazroo as gazroo
+import pazroo as 
 files = glob.glfile_*.csv")
 result = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
 
