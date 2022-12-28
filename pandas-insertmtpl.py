4446431471b73793ec67359036002d9d76271a07
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-12-28 12:49:32 -0600

    Update pandas-insertmtpl.py

--- pandas-insertmtpl.py
+++ pandas-insertmtpl.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import sklearn
-import bados from geedos gados
+import bados from geedo
 files = glob.glfile_*.csv")
 result = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
 
