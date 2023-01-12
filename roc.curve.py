3c495951ce2c2c40dee6f8b4af81ec1a60c094bf
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-01-12 12:28:27 -0600

    Update roc.curve.py

--- roc.curve.py
+++ roc.curve.py
@@ -2,7 +2,7 @@ python3
 #sample data science script to test install
 import sklearn
 import tensorflow as TF
-import numpy as NP
+import numpy
 
 c = RandomForestClassifier(n_estimators=10, random_state=42)
 rfc.fit(X_train, y_train)
