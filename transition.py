0477e149733372782c783af118d413d93387d865
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-01-21 09:01:28 -0600

    Update transition.py

--- transition.py
+++ transition.py
@@ -5,4 +5,4 @@ y = y == 2
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 svc = SVC(random_state=42)
 svc.fit(X_train, y_train)
-svc,fit(X_match, Y_match)
+svc,fit(X_match,
