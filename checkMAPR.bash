470e5dee6d245e634740223954d1a7d50bdab719
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-05-13 08:40:04 -0500

    Create checkMAPR.bash

---
+++ checkMAPR.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the nodes of the cluster
+NODES=( node1 node2 node3 node4 node5 node6 node7 node8 node9 node10 )
+
+# Iterate over the nodes and check if the disks are mounted
+for NODE in "${NODES[@]}"
+do
+  echo "Checking disks on node $NODE..."
+  ssh $NODE df -h | grep /dev/sd
+done
