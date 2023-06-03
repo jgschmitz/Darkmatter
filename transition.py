52a69cf89d8c20bc9833c8954d125a8475582ad1
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-06-03 08:51:43 -0500

    Update and rename transition.py to train.model.tf

--- transition.py
+++
@@ -1,7 +0,0 @@
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-X, y = load_wine(return_X_y=True)
-y = y == 2
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-svc = SVC(random_state=42)
-svc.fit(X_train, y_train)
