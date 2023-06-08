478cca5a0552e07b77786f893e0547ae081c3f2b
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-06-08 09:00:30 -0500

    Update kafla-build-mapr.js

--- kafla-build-mapr.js
+++ kafla-build-mapr.js
@@ -1,5 +1,25 @@
-name=mapr-to-s3-pipeline
-connector.class=com.mapr.kafka.connect.file.MapRSourceConnector
-tasks.max=1
-mapr.source.path=/path/to/mapr/file
-topic=my-topic
+{
+  "name": "maprfs-source",
+  "config": {
+    "connector.class": "io.confluent.connect.mapr.fs.MapRfsSourceConnector",
+    "tasks.max": "1",
+    "maprfs.source.path": "/path/to/maprfs",
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+    "value.converter.schemas.enable": "false",
+    "topics": "maprfs-topic"
+  }
+}
+
+{
+  "name": "s3-sink",
+  "config": {
+    "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+    "tasks.max": "1",
+    "topics": "maprfs-topic",
+    "s3.bucket.name": "your-s3-bucket",
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+    "value.converter.schemas.enable": "false"
+  }
+}
