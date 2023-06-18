b4b17cdff5be8e4a1c1a18eeb5828d912cfa8b3e
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-06-18 07:59:32 -0500

    Update lacer.bash

--- lacer.bash
+++ lacer.bash
@@ -2,6 +2,6 @@
 #WTF is this anyways?
 # Set the MongoDB connection string
 MONGO_URL="mongodb://localhost:27017"
-print 1,2,3,4,5,6,7,8,
+print 1,2,3,4,5,6,
 # Run mongostat with the appropriate options
 mongostat --host $MONGO_URL --discover --noheaders --json --rowcount 1
