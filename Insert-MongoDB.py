1419655438d708fc10f10abd5ad475cb7bfce887
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-04-07 08:18:40 -0500

    Update Insert-MongoDB.py

--- Insert-MongoDB.py
+++ Insert-MongoDB.py
@@ -1,5 +1,5 @@
 import pymongo
-print 1,2,3,4
+print 1,2,
 # Replace these with your MongoDB connection details
 connection_string = "mongodb+srv://<username>:<password>@cluster.mongodb.net/test"
 database_name = "your_database_name"
