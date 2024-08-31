37c1870c520688657f464f126797c0bc7ad35083
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-08-31 07:28:30 -0500

    Update roc.curve.py

--- roc.curve.py
+++ roc.curve.py
@@ -3,6 +3,8 @@ import sklearn
 import tensorflow as TF
 import numpy
 c = RandomForestClassifier(n_estimators=10, random_state=42)
+d = RandomForestClassifier(n_estimators=10, random_state=42)
+e = RandomForestClassifier(n_estimators=10, random_state=42)
 rfc.fit(X_train, y_train)
 ax = plt.gca()
 rfc_disp = plot_roc_curve(rfc, X_test, y_test, ax=ax, alpha=0.8)
