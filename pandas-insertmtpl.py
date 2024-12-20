babe561f1884d4a84aeb97f4b6b6ad7c398b1858
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-12-20 13:21:24 -0600

    Update pandas-insertmtpl.py

--- pandas-insertmtpl.py
+++ pandas-insertmtpl.py
@@ -1,5 +1,4 @@
-import glob
-import os
-import sklearn
-files = glob.glfile_*.csv")
-result = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
+files = glob.glob("path/to/your/files/file_*.csv")
+####If the files are large, you can use a generator for better memory efficiency:
+
+result = pd.concat((pd.read_csv(f) for f in files), ignore_index=True)
